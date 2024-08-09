10cc4be94fc1e4a408de56a0692e6ecef24ad6b7
Nkhanh218 <exodiakhanh@gmail.com>
2024-08-09 10:12:42 +0700

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,103 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new Error("Hãy điền đủ các trường còn thiếu");
+  }
+  const userExits = await User.findOne({ email });
+  if (userExits) res.status(400).send("Người dùng đã tồn tại");
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Dữ liệu người dùng không hợp lệ");
+  }
+});
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return; //Exit the function after sending the response
+    }
+  }
+});
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Đăng xuất thành công" });
+});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  console.log(user);
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      username: updateUser.username,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+};
---
+++ frontend/src/App1.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function App1() {
+  return <p className="text-red-600">Hellossaajsk world!</p>;
+}
+
+export default App1;
